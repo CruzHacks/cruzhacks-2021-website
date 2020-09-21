1c364aa1f21cf8bf022e77dc0688d9a72c344a7c
Isai Lopez <ilopezro@ucsc.edu>
2020-09-21 10:53:38 -0700

    added more boilerplate code for typescript and got workflow working

---
+++ src/landing/mission/mission.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MissionSection: React.FC = () => {
+  return <>Mission Component Goes Here</>;
+};
+
+export default MissionSection;
---
+++ src/landing/sponsor/sponsor.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SponsorSection: React.FC = () => {
+  return <>Sponsor Component Goes Here</>;
+};
+
+export default SponsorSection;
