05adb1d0b744c91afb586d3f45ebff80d76f038d
Isai Lopez Rodas <40477553+ilopezro@users.noreply.github.com>
2020-10-16 13:11:17 -0700

    Added Hero Component (#8)
    * added hero view and images folder
    
    * implemented background
    
    * added additional props to hero view
    
    * added arrow svg & nested the css props

--- src/components/PostcardStack/index.view.tsx
+++ src/components/PostcardStack/index.view.tsx
@@ -1,8 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./PostcardStack.scss";
 
-const PostcardStack: React.FC = () => {
-  return <div className="PostcardStack">Hello from PostcardStack</div>;
+type PostcardStackProps = {
+  pageName: string;
+};
+
+const PostcardStack: React.FC<PostcardStackProps> = ({ pageName }) => {
+  return (
+    <div className="PostcardStack">
+      Hello from PostcardStack sent from {pageName}
+    </div>
+  );
 };
 
 export default PostcardStack;
+
+PostcardStack.propTypes = {
+  pageName: PropTypes.string.isRequired,
+};
--- src/views/Home/index.view.tsx
+++ src/views/Home/index.view.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import HeroView from "views/Hero/index.view";
+import Navbar from "components/Navbar/index.view";
 
 import "./Home.scss";
 
 const HomepageView: React.FC = () => {
-  return <div className="Homepage">Hello from HomepageView</div>;
+  return (
+    <div className="Homepage">
+      <Navbar />
+      <HeroView pageName={"Home"} title={"Title"} description={"Description"} />
+    </div>
+  );
 };
 
 export default HomepageView;
