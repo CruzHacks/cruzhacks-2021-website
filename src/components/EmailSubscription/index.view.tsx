e31bb975dc125051302aa380fe688c7ef09e4c7e
Tim <tim.nguyen.cs@gmail.com>
2020-10-21 00:20:26 -0700

    handle user input

--- src/components/EmailSubscription/index.view.tsx
+++ src/components/EmailSubscription/index.view.tsx
@@ -1,13 +1,29 @@
 import React from "react";
-import { ReactComponent as Arrow } from 'images/arrow.svg';
+import { ReactComponent as Arrow } from "images/arrow.svg";
 
 import "./EmailSubscription.scss";
 
 const EmailSubscriptionForm: React.FC = () => {
+  var email: string = ""
+
+  const handleInput = (event: React.FormEvent<HTMLInputElement>) => {
+    email = event.currentTarget.value.trim()
+  }
+
+  const handleKey = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter")
+      submitEmail()
+  }
+
+  const submitEmail = (event?: React.MouseEvent<HTMLButtonElement>) => {
+    console.log("We should submit the email!")
+    console.log(email)
+  }
+
   return (
     <div className="EmailSubscription">
-      <input className="SearchBox" placeholder="enter email for updates..." type="text" name="name"></input>
-      <div className="InputButton"><Arrow /></div>
+      <input className="SearchBox" placeholder="enter email for updates..." type="text" name="name" onChange={handleInput} onKeyDown={handleKey}></input>
+      <button className="InputButton" onClick={submitEmail}><Arrow /></button>
     </div>
   );
 };
