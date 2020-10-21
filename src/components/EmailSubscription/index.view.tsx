e9ed21da4223269e0b97f8396e712bd1ace53067
Tim <tim.nguyen.cs@gmail.com>
2020-10-20 23:20:48 -0700

    finished email subscription layout

--- src/components/EmailSubscription/index.view.tsx
+++ src/components/EmailSubscription/index.view.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { ReactComponent as Arrow } from 'images/arrow.svg';
+
 import "./EmailSubscription.scss";
 
 const EmailSubscriptionForm: React.FC = () => {
   return (
-    <div className="EmailSubscription">Hello From EmailSubscriptionForm</div>
+    <div className="EmailSubscription">
+      <input className="SearchBox" placeholder="enter email for updates..." type="text" name="name"></input>
+      <div className="InputButton"><Arrow /></div>
+    </div>
   );
 };
 
