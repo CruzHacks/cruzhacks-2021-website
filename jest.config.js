c7d6da89902292e64f5f9b32769790e10624c9da
Isai Lopez <ilopezro@ucsc.edu>
2020-10-24 21:48:43 -0700

    removed unnecessary dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   collectCoverageFrom: ["src/**/*.{ts,tsx}"],
-  moduleNameMapper: {
-    "\\.(css|sass|scss|erb)$": "<rootDir>/src/__mocks__/fileMock.ts",
-    "\\.svg$": "<rootDir>/src/__mocks__/svgMock.ts",
-  },
   modulePaths: ["src/"],
   reporters: [
     "default",
@@ -12,7 +8,5 @@ module.exports = {
       { suiteName: "jest", outputDirectory: "<rootDir>/tmp/test-results" },
     ],
   ],
-  setupFilesAfterEnv: ["jest-enzyme", "<rootDir>/src/setupTests.ts"],
-  testEnvironment: "enzyme",
   testPathIgnorePatterns: ["node_modules"],
 };
