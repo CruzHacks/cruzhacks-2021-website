6aaba2429df8eb12987c344ac53dd3542b2eeebd
tim-nguyen-cs <33771623+tim-nguyen-cs@users.noreply.github.com>
2020-11-01 10:43:25 -0800

    Adding Email Subscription Component (#11)
    * updated arrow SVG and minimum breakpoint
    
    * finished email subscription layout
    
    * handle user input
    
    * configured mailing service with Axios
    
    * fixed on lint error and PR comments
    
    * resolved merged styles
    
    * revised SCSS nesting
    
    * added feedback to mailing component
    
    * Set up Environment Variables (#16)
    
    * added webpack config for env vars
    
    * added react app endpoint
    
    * added sample env
    
    * endline for env file
    
    * extrapolated API call to env-var
    
    * integrated EmailSubscription into Hero context
    
    * improved mobile responsiveness
    
    * responsive to large mobile screens (#17)
    
    * email input & send button are more compact w/o flex-wrap
    
    * refactor input to be stateless since we need simple HTML5 form validation
    
    * refactor hero & postcard responsiveness to phone
    
    still not responsive to phone-narrow & galaxy fold breakpoint
    
    * feedback text is more visible & error message is contextual
    
    we should convert the "contact us" text to a link
    
    * replace placeholder text
    
    * increase feedback message visibility using font-weight
    
    * trim description text for responsiveness
    
    * added console error and deleted package lock
    
    * disregard error
    
    * removed array for descriptions
    
    * workflow against feature branches
    
    * refactored code to make hero component reusable
    
    Co-authored-by: Isai Lopez <ilopezro@ucsc.edu>
    
    * added email to feedbackMessage
    
    Co-authored-by: Isai Lopez Rodas <40477553+ilopezro@users.noreply.github.com>
    Co-authored-by: Eric Mockler <emockler@ucsc.edu>
    Co-authored-by: Isai Lopez <ilopezro@ucsc.edu>

--- src/components/EmailSubscription/index.view.tsx
+++ src/components/EmailSubscription/index.view.tsx
@@ -1,9 +1,70 @@
-import React from "react";
-import "./EmailSubscription.scss";
+import React, { useState } from "react";
+import axios, { AxiosResponse } from "axios";
+import { ReactComponent as Arrow } from "images/arrow.svg";
 
+import "./EmailSubscription.scss";
+const subscriptionEndpoint = process.env.REACT_APP_SUBSCRIBE_ENDPOINT || "";
 const EmailSubscriptionForm: React.FC = () => {
+  const [userEmail, setUserEmail] = useState("");
+  const [feedbackMessage, setFeedbackMessage] = useState("");
+  const [showFeedback, setShowFeedback] = useState(true);
+  const [showInput, setShowInput] = useState(true);
+
+  const submitEmail = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setShowFeedback(true);
+    setFeedbackMessage("Please wait while your message is being submitted...");
+    setUserEmail("");
+
+    axios
+      .post(subscriptionEndpoint, {
+        email: userEmail,
+      })
+      .then((response: AxiosResponse) => {
+        setFeedbackMessage(response.data.message);
+
+        if (response.status === 200) {
+          setShowInput(false);
+        }
+      })
+      .catch(() => {
+        setFeedbackMessage(
+          "Oh no! We've got an error— please try your request again & contact us at dev@cruzhacks.com if this persists!"
+        );
+      });
+  };
+
   return (
-    <div className="EmailSubscription">Hello From EmailSubscriptionForm</div>
+    <>
+      {showInput && (
+        <form onSubmit={(event) => submitEmail(event)}>
+          <div className="EmailSubscription">
+            <input
+              className="EmailSubscription__searchBox"
+              placeholder="enter email for updates…"
+              aria-label="enter your email address for updates"
+              type="email"
+              name="email input"
+              required
+            />
+            <button
+              className="EmailSubscription__inputButton"
+              type="submit"
+              aria-label="send button"
+            >
+              <Arrow className="EmailSubscription__arrow" />
+            </button>
+          </div>
+        </form>
+      )}
+      {showFeedback && (
+        <div className="EmailSubscription__feedbackContainer">
+          <span className="EmailSubscription__feedbackMessage">
+            {feedbackMessage}
+          </span>
+        </div>
+      )}
+    </>
   );
 };
 
