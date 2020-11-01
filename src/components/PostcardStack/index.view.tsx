fdcb2fa2d9574c6a6e4be0912b533abca326d0bb
Isai Lopez Rodas <40477553+ilopezro@users.noreply.github.com>
2020-10-31 17:32:36 -0700

    Setup Testing Suite (#15)
    * removed unnecessary dependencies
    
    * init testing suite
    
    * added first test to see how it runs on ci
    
    * modified service worker to fix deepcode ai failure in prod
    
    * added more tests
    
    * added headless true
    
    * increased timeout
    
    * added react jsx transformer
    
    * fixed babel config file
    
    * added serve
    
    * added babel plugins
    
    * added yarn start command again
    
    * added serve again and removed passWOTest flag
    
    * added path
    
    * fixed issue
    
    * deepcode fix?
    
    * added init test for all views
    
    * collect coverage from js files only
    
    * added sample tests
    
    * added more tests
    
    * changed test name

--- src/components/PostcardStack/index.view.tsx
+++ src/components/PostcardStack/index.view.tsx
@@ -5,16 +5,40 @@ interface PostcardStackProps {
   pageName: string;
 }
 
+interface Postcard {
+  name: string;
+  image: string;
+  altText: string;
+}
+
+const postcards: Postcard[] = [
+  {
+    name: "Home",
+    image: require("images/postcards/home.svg"),
+    altText: "The winding road to the UCSC campus.",
+  },
+  {
+    name: "Information",
+    image: require("images/postcards/home.svg"),
+    altText: "The winding road to the UCSC campus.",
+  },
+];
 const PostcardStack: React.FC<PostcardStackProps> = ({
   pageName,
 }: PostcardStackProps) => {
   return (
     <div className="PostcardStack">
-      <img
-        src={require(`images/postcards/${pageName.toLowerCase()}.svg`)}
-        className="PostcardStack__postcard"
-        alt="The winding road to the UCSC campus."
-      />
+      {postcards
+        .filter((item) => item.name === pageName)
+        .map(({ name, image, altText }) => (
+          <img
+            src={image}
+            className="PostcardStack__postcard"
+            alt={altText}
+            key={name}
+            id={`${name}-postcard`}
+          />
+        ))}
     </div>
   );
 };
---
+++ src/views/Home/Home.test.js
@@ -0,0 +1,18 @@
+const baseUrl = process.env.false || "http://localhost:3000";
+
+describe("Homepage", () => {
+  beforeAll(async () => {
+    await page.goto(baseUrl);
+  });
+
+  it("displays Homepage with appropriate components", async () => {
+    const homepageDiv = await page.$(".Homepage");
+    expect(homepageDiv).not.toBeNull();
+    const heroHomepageDiv = await page.$(".HeroView__homeView");
+    expect(heroHomepageDiv).not.toBeNull();
+    const postcardStack = await homepageDiv.$(".PostcardStack");
+    expect(postcardStack).not.toBeNull();
+    const homePostcard = await postcardStack.$("#Home-postcard");
+    expect(homePostcard).not.toBeNull();
+  });
+});
