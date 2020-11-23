de2b2b8daa9e28079778d295f9c6826ab4df55a1
tim-nguyen-cs <33771623+tim-nguyen-cs@users.noreply.github.com>
2020-11-23 14:32:20 -0800

    adding buttons to website body (#64)
    * finish first iteration
    
    * created buttons!
    
    * removed navBar + added buttons in Body
    
    * optimized code to use map() and revised links

---
+++ src/components/Button/index.view.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Button.scss";
+
+export interface ButtonProps {
+  text: string;
+  link: string;
+  label: string;
+}
+
+const Button: React.FC<ButtonProps> = ({ text, link, label }: ButtonProps) => {
+  return (
+    <a className="Button" href={link} target="_blank" rel="noreferrer">
+      <button className="Button__element" aria-label={label}>
+        {text}
+      </button>
+    </a>
+  );
+};
+
+export default Button;
--- src/views/Home/index.view.tsx
+++ src/views/Home/index.view.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import HeroView from "views/Hero/index.view";
+// import Navbar from "components/Navbar/index.view";
+import Button from "components/Button/index.view";
 import EmailSubscriptionForm from "components/EmailSubscription/index.view";
-
 import "./Home.scss";
 
 const title = [
@@ -25,12 +26,37 @@ const description = [
   },
 ];
 
+const buttonProps = {
+  hacker: {
+    text: "Apply to hack!",
+    link: "https://bit.ly/cruzhacks-2021-hacker",
+    label: "hacker sign-up button",
+  },
+  judge_mentor: {
+    text: "Be a judge / mentor!",
+    link:
+      "https://docs.google.com/forms/d/e/1FAIpQLScVmW-gIcKGrp7fBVeH5i8G1VQ83IwmKrkIo_J2PeRj3PRYTw/viewform",
+    label: "judge/mentor sign-up button",
+  },
+  sponsor: {
+    text: "Be a sponsor!",
+    link:
+      "https://drive.google.com/file/d/127G6lE1SFVQUeC3wSMdMwn12E9sVFCyU/view",
+    label: "sponsor sign-up button",
+  },
+};
+
 const HomepageView: React.FC = () => {
   return (
     <div className="Homepage">
       <HeroView pageName={"Home"} title={title} description={description}>
         <div className="Homepage__emailSubscriptionContainer">
           <EmailSubscriptionForm />
+        </div>
+        <div className="Homepage__buttonContainer">
+          {Object.entries(buttonProps).map(([_, buttonProp]) => (
+            <Button key={buttonProp.text} {...buttonProp} />
+          ))}
         </div>
       </HeroView>
     </div>
