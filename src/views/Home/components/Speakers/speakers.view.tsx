3697261f21f1497216814c55c7ac8ccae6f1fafd
Isai Lopez Rodas <40477553+ilopezro@users.noreply.github.com>
2020-11-28 22:59:22 -0800

    Added MLH Badge, New Favicon, & New Scaffolding (#83)
    This commit is in preparation for the work that'll be done for the new site!

---
+++ src/views/Home/components/Speakers/speakers.view.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./speakers.scss";
+
+const SpeakersComponent: React.FC = () => {
+  return <div className="Speakers">Hello from SpeakersComponent</div>;
+};
+
+export default SpeakersComponent;
