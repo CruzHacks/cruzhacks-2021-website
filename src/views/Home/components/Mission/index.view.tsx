f85dff04b412a75a26a4da4174804838a6cf72f7
Tim <tim.nguyen.cs@gmail.com>
2020-12-04 19:38:55 -0800

    finished declaring + styling mission statements

--- src/views/Home/components/Mission/index.view.tsx
+++ src/views/Home/components/Mission/index.view.tsx
@@ -1,8 +1,36 @@
 import React from "react";
 import "./mission.scss";
 
-const MissionComponent: React.FC = () => {
-  return <div className="Mission">Hello From MissionComponent</div>;
+interface MissionProps {
+  about_text: { [key: string]: string };
+  mission_text: { [key: string]: string };
+}
+
+const MissionComponent: React.FC<MissionProps> = ({
+  about_text,
+  mission_text,
+}: MissionProps) => {
+  return (
+    <div className="Mission">
+      <div className="Mission__section">
+        <div className="Mission__highlight" />
+        <div className="Mission__section--sub-container">
+          <div className="Mission__title">{about_text.title}</div>
+          <div className="Mission__body">
+            <div className="Mission__rectangle" />
+            <div className="Mission__text">{about_text.body}</div>
+          </div>
+        </div>
+      </div>
+      <div className="Mission__section">
+        <div className="Mission__title">{mission_text.title}</div>
+        <div className="Mission__body">
+          <div className="Mission__rectangle" />
+          <div className="Mission__text">{mission_text.body}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MissionComponent;
--- src/views/Home/index.view.tsx
+++ src/views/Home/index.view.tsx
@@ -1,48 +1,18 @@
 import * as React from "react";
-import HeroView from "views/Hero/index.view";
-import Button from "components/Button/index.view";
-import EmailSubscriptionForm from "components/EmailSubscription/index.view";
+import Mission from "views/Home/components/Mission/index.view";
 import MLHBanner from "components/MLHBanner/index.view";
 import "./Home.scss";
 
-const title = [
-  {
-    text: "CruzHacks 2021 /",
-    style: "",
+const mission_text = {
+  about: {
+    title: "We Are CruzHacks",
+    body:
+      "CruzHacks is a virtual hackathon that provides ground for new ideas and innovation to flourish. By presenting real-world problems, hackers have the chance to begin a project using their creative and problem-solving technical skills. Open to individuals in varying levels of experience, Cruzhacks provides the opportunity to apply the skills learned in classes and connect with companies through the product that has been created.",
   },
-  {
-    text: "January 15-17",
-    style: "--secondary",
-  },
-];
-
-const description = [
-  {
-    description:
-      "CruzHacks is the largest hackathon in Santa Cruz. Each year, we invite hundreds of students to develop solutions to real-world problems, pursue inclusion in tech, and kindle the spirit of innovation.  ",
-  },
-  {
-    description: "This year, we’re bringing CruzHacks to you.  ",
-  },
-];
-
-const buttonProps = {
-  hacker: {
-    text: "Apply to hack!",
-    link: "https://bit.ly/cruzhacks-2021-hacker",
-    label: "hacker sign-up button",
-  },
-  judge_mentor: {
-    text: "Be a judge / mentor!",
-    link:
-      "https://docs.google.com/forms/d/e/1FAIpQLScVmW-gIcKGrp7fBVeH5i8G1VQ83IwmKrkIo_J2PeRj3PRYTw/viewform",
-    label: "judge/mentor sign-up button",
-  },
-  sponsor: {
-    text: "Be a sponsor!",
-    link:
-      "https://drive.google.com/file/d/127G6lE1SFVQUeC3wSMdMwn12E9sVFCyU/view",
-    label: "sponsor sign-up button",
+  mission: {
+    title: "The CruzHacks Mission",
+    body:
+      "CruzHacks is a global event where people can collaborate, network, and apply technology to solve problems! Our initiative is to expand diversity in tech while facilitating an environment conducive for creating innovative solutions to complex problems. The CruzHacks mission, to support creativity in technology and promote the development solutions for social good, are at the forefront of our efforts every year.",
   },
 };
 
@@ -51,20 +21,10 @@ const HomepageView: React.FC = () => {
     <>
       <MLHBanner />
       <div className="Homepage">
-        <HeroView pageName={"Home"} title={title} description={description}>
-          <div className="Homepage__emailSubscriptionContainer">
-            <EmailSubscriptionForm />
-          </div>
-          <div className="Homepage__buttonContainer">
-            {Object.entries(buttonProps).map(([_, buttonProp]) => (
-              <Button
-                key={buttonProp.text}
-                className="Homepage__button"
-                {...buttonProp}
-              />
-            ))}
-          </div>
-        </HeroView>
+        <Mission
+          about_text={mission_text.about}
+          mission_text={mission_text.mission}
+        />
       </div>
     </>
   );
