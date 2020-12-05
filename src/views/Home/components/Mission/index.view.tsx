860eba9a35d607557ccc4723310096f92e950a8f
Tim <tim.nguyen.cs@gmail.com>
2020-12-04 23:14:06 -0800

    preliminary image draft done

--- src/views/Home/components/Mission/index.view.tsx
+++ src/views/Home/components/Mission/index.view.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { ReactComponent as LeftCan } from "images/mission-can-left.svg";
+import { ReactComponent as RightCan } from "images/mission-can-right.svg";
+import { ReactComponent as Flower } from "images/mission-flower.svg";
 import "./mission.scss";
 
 interface MissionProps {
@@ -12,22 +15,29 @@ const MissionComponent: React.FC<MissionProps> = ({
 }: MissionProps) => {
   return (
     <div className="Mission">
-      <div className="Mission__section">
-        <div className="Mission__highlight" />
-        <div className="Mission__section--sub-container">
-          <div className="Mission__title">{about_text.title}</div>
+      <div className="Mission--text">
+        <div className="Mission__section">
+          <div className="Mission__highlight" />
+          <div className="Mission__section--sub-container">
+            <div className="Mission__title">{about_text.title}</div>
+            <div className="Mission__body">
+              <div className="Mission__rectangle" />
+              <div className="Mission__text">{about_text.body}</div>
+            </div>
+          </div>
+        </div>
+        <div className="Mission__section">
+          <div className="Mission__title">{mission_text.title}</div>
           <div className="Mission__body">
             <div className="Mission__rectangle" />
-            <div className="Mission__text">{about_text.body}</div>
+            <div className="Mission__text">{mission_text.body}</div>
           </div>
         </div>
       </div>
-      <div className="Mission__section">
-        <div className="Mission__title">{mission_text.title}</div>
-        <div className="Mission__body">
-          <div className="Mission__rectangle" />
-          <div className="Mission__text">{mission_text.body}</div>
-        </div>
+      <div className="Mission--images">
+        <LeftCan className="Mission__can" />
+        <RightCan className="Mission__can--right" />
+        <Flower className="Mission__flower" />
       </div>
     </div>
   );
