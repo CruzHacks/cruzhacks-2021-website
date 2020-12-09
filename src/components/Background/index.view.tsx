29277d4137916b8244704044cc351b348ac17b31
mocklee <mock7ee@gmail.com>
2020-12-09 15:23:55 -0800

    add negative space before merging in sections to visualize background

--- src/components/Background/index.view.tsx
+++ src/components/Background/index.view.tsx
@@ -12,7 +12,6 @@ const Background: React.FC<BackgroundProps> = ({
   return (
     <div className="Background">
       <Wire className="Background__wire"></Wire>
-      <div className="Background__gradient"></div>
       <div className="Background__children">{children}</div>
       <Rainbow className="Background__wire--variant"></Rainbow>
     </div>
