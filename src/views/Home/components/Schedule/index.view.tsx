88a6073c96b263a37310f8b6479908c9007ab6a5
Priya Rajarathinam <cat@jjjjjjj.attlocal.net>
2020-12-12 14:21:23 -0800

    in progress for the base shapes of the schedule

--- src/views/Home/components/Schedule/index.view.tsx
+++ src/views/Home/components/Schedule/index.view.tsx
@@ -1,20 +1,22 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-import { useState } from "react";
+// import { useState } from "react";
 import * as React from "react";
 import "./schedule.scss";
 
 const ScheduleComponent: React.FC = () => {
-  const [selectedDay] = useState(1);
+  // const [selectedDay] = useState(1);
   return (
     <div className="Schedule">
       Hello from ScheduleComponent
-      <img
-        src={require(`images/schedule/day${selectedDay}.svg`).default}
-        className="Schedule__schedule"
-        alt="schedule"
-        key="schedule"
-        id="schedule"
-      />
+      <div className="Schedule__container">
+        <div className="Schedule__whiteRect">
+          <div className="Schedule__activeTab Schedule__dayOne"></div>
+          <div className="Schedule__activeTab Schedule__dayTwo"></div>
+          <div className="Schedule__activeTab Schedule__dayThree"></div>
+        </div>
+        <div className="Schedule__pinkRect"></div>
+        <div className="Schedule__whiteRect--variant"></div>
+      </div>
       <img
         src={require("images/schedule/moon.svg").default}
         className="Schedule__moon"
