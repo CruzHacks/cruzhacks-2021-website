38b7d4e4ada281036af053db9379b7071aa89df0
mocklee <mock7ee@gmail.com>
2020-12-19 16:16:47 -0800

    lint scss & update hero test case

--- src/views/Home/Home.test.js
+++ src/views/Home/Home.test.js
@@ -8,7 +8,7 @@ describe("Homepage", () => {
   it("displays Homepage with appropriate components", async () => {
     const homepageDiv = await page.$(".Homepage");
     expect(homepageDiv).not.toBeNull();
-    const heroHomepageDiv = await page.$(".Hero__homeView");
+    const heroHomepageDiv = await page.$(".Hero");
     expect(heroHomepageDiv).not.toBeNull();
     const postcardStack = await homepageDiv.$(".PostcardStack");
     expect(postcardStack).not.toBeNull();
