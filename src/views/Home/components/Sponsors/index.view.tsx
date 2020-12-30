a8b45a022377a22a971acf2c157826f08c6009be
mocklee <mock7ee@gmail.com>
2020-12-29 21:26:07 -0800

    style sponsor header text
    also move `powered by` text to team section

--- src/views/Home/components/Sponsors/index.view.tsx
+++ src/views/Home/components/Sponsors/index.view.tsx
@@ -16,9 +16,6 @@ const SponsorsComponent: React.FC<Sponsors> = () => {
       <div className="Sponsors__titleContainer">
         <h2 className="Sponsors__title">Thanks to Our Sponsors</h2>
         <hr className="Sponsors__ruleLine" />
-        <h2 className="Sponsors__title">
-          Powered by Blackstone Launchpad & CIED & Techstars
-        </h2>
       </div>
       <div className="Sponsors__logoContainer"></div>
     </div>
