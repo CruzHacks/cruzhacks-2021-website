a08de6eaa147f49452ba5a66bf0370b0ee49312b
Eric Mockler <emockler@ucsc.edu>
2020-12-30 17:01:22 -0800

    Sponsor logos (#139)
    * boilerplate JSX for sponsors
    
    * add sponsor logos & serialize JSON
    
    * lint & refactor smoke test
    
    * remove npm package lock
    
    * lint hero.scss
    
    * update smoke test
    
    * fix logo type
    
    * fix anchor links with specific z-indexing in background
    
    * style sponsor header text
    
    also move `powered by` text to team section
    
    * svg rendering through path prop
    
    * load all sponsor SVGs in DOM
    
    * logos responsive to tab-land
    
    * add anaglyph to footer text
    
    * undo anaglyph for better contrast
    
    * logos responsive to tab-port
    
    * logos fully responsive
    
    * background wire variant responsive with sponsors
    
    * kilo sponsors aligned
    
    * lint sponsors.scss
    
    * lint sponsors.scss
    
    * adjust background grid across hero & milestones

--- src/views/Home/index.view.tsx
+++ src/views/Home/index.view.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import Hero from "views/Home/components/Hero/index.view";
-import Footer from "./components/Footer/index.view";
 import Button from "components/Button/index.view";
 import EmailSubscriptionForm from "components/EmailSubscription/index.view";
 import MLHBanner from "components/MLHBanner/index.view";
@@ -8,6 +7,23 @@ import Background from "components/Background/index.view";
 import Mission from "views/Home/components/Mission/index.view";
 import "./Home.scss";
 import MilestonesComponent from "./components/Milestones/index.view";
+import SponsorsComponent from "./components/Sponsors/index.view";
+import FooterComponent from "./components/Footer/index.view";
+
+// sponsor logo paths
+import Balsamiq from "images/sponsors/balsamiq.svg";
+import CIED from "images/sponsors/cied.svg";
+import CITRIS from "images/sponsors/citris.svg";
+import ChooseSantaCruz from "images/sponsors/chooseSantaCruz.svg";
+import Comcast from "images/sponsors/comcast.svg";
+import eBay from "images/sponsors/ebay.svg";
+import MLH from "images/sponsors/mlh.svg";
+import Pinnacle from "images/sponsors/pinnacle.svg";
+import SouthSwell from "images/sponsors/southSwell.svg";
+import Stickermule from "images/sponsors/stickermule.svg";
+import Stratovan from "images/sponsors/stratovan.svg";
+import Wolfram from "images/sponsors/wolfram.svg";
+import FifteenSeventeen from "images/sponsors/1517.svg";
 
 const title = [
   {
@@ -79,34 +95,118 @@ const mission_props = {
   },
 };
 
+const sponsors = [
+  {
+    name: "Center for Innovation and Entrepreneurial Development",
+    className: "cied",
+    logo: CIED,
+    url: "https://cied.ucsc.edu/",
+  },
+  {
+    name: "Comcast",
+    className: "comcast",
+    logo: Comcast,
+    url: "https://www.nbcuniversal.com/about",
+  },
+  {
+    name: "1517",
+    className: "fifteenSeventeen",
+    logo: FifteenSeventeen,
+    url: "https://www.1517fund.com/",
+  },
+  {
+    name:
+      "Center for Information Technology Research in the Interest of Society",
+    className: "citris",
+    logo: CITRIS,
+    url: "https://citris-uc.org/",
+  },
+  {
+    name: "eBay",
+    className: "ebay",
+    logo: eBay,
+    url: "https://www.ebayinc.com/company/",
+  },
+  {
+    name: "South Swell Ventures",
+    className: "southSwell",
+    logo: SouthSwell,
+    url: "https://www.facebook.com/South-Swell-Ventures-422070777891517/",
+  },
+  {
+    name: "Stratovan",
+    className: "stratovan",
+    logo: Stratovan,
+    url: "https://www.stratovan.com/",
+  },
+  {
+    name: "Balsamiq, LLC",
+    className: "balsamiq",
+    logo: Balsamiq,
+    url: "https://balsamiq.com/",
+  },
+  {
+    name: "Major League Hacking",
+    className: "mlh",
+    logo: MLH,
+    url: "https://mlh.io/",
+  },
+  {
+    name: "Choose Santa Cruz",
+    className: "chooseSC",
+    logo: ChooseSantaCruz,
+    url: "https://www.choosesantacruz.com/",
+  },
+  {
+    name: "Wolfram Language",
+    className: "wolfram",
+    logo: Wolfram,
+    url: "https://www.wolfram.com/language/",
+  },
+  {
+    name: "Stickermule",
+    className: "stickermule",
+    logo: Stickermule,
+    url: "https://www.stickermule.com/",
+  },
+  {
+    name: "Pinnacle",
+    className: "pinnacle",
+    logo: Pinnacle,
+    url: "https://pinnacle.us.org/",
+  },
+];
+
 const HomepageView: React.FC = () => {
   return (
     <>
       <MLHBanner />
       <div className="Homepage">
-        <Background />
-        <Hero pageName={"Home"} title={title} description={description}>
-          <div className="Homepage__emailSubscriptionContainer">
-            <EmailSubscriptionForm />
-          </div>
-          {Object.entries(buttonProps).map(([type, props]) => (
-            <div key={type} className="Homepage__buttonContainer">
-              {Object.entries(props).map(([_, buttonProp]) => (
-                <Button
-                  key={buttonProp.text}
-                  className={"Homepage__button"}
-                  {...buttonProp}
-                />
-              ))}
+        <Background>
+          <Hero pageName={"Home"} title={title} description={description}>
+            <div className="Homepage__emailSubscriptionContainer">
+              <EmailSubscriptionForm />
             </div>
-          ))}
-        </Hero>
-        <Mission
-          about_text={mission_props.about}
-          mission_text={mission_props.mission}
-        />
-        <MilestonesComponent />
-        <Footer />
+            {Object.entries(buttonProps).map(([type, props]) => (
+              <div key={type} className="Homepage__buttonContainer">
+                {Object.entries(props).map(([_, buttonProp]) => (
+                  <Button
+                    key={buttonProp.text}
+                    className={"Homepage__button"}
+                    {...buttonProp}
+                  />
+                ))}
+              </div>
+            ))}
+          </Hero>
+          <Mission
+            about_text={mission_props.about}
+            mission_text={mission_props.mission}
+          />
+          <MilestonesComponent />
+          <SponsorsComponent sponsors={sponsors} />
+          <FooterComponent />
+        </Background>
       </div>
     </>
   );
