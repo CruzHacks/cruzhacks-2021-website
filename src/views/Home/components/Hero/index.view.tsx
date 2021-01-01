82d7f7c8b70a345a2982c23677631d23f5eef7a3
Eric Mockler <emockler@ucsc.edu>
2021-01-01 12:18:24 -0800

    Hero component  (#122)
    * integrate milestones component
    
    * add salmon to hero
    
    * button text more visible
    
    * hero background grid responsive
    
    * laptop/grid/postcard graphics responsive
    
    * reset line height on buttons to toptimize margins
    
    * change hero relative width unit to fix x-overflow
    
    * add mouse graphic
    
    * large flare responsive
    
    * lens flares responsive
    
    * add padding to hero text container
    
    * drop in lottie with postcard animation
    
    * use shorthand syntax for object literal
    
    * lint & refactor smoke test
    
    * remove npm package lock
    
    * lint hero.scss
    
    * update smoke test
    
    * Sponsor logos (#139)
    
    * boilerplate JSX for sponsors
    
    * add sponsor logos & serialize JSON
    
    * lint & refactor smoke test
    
    * remove npm package lock
    
    * lint hero.scss
    
    * update smoke test
    
    * fix logo type
    
    * fix anchor links with specific z-indexing in background
    
    * style sponsor header text
    
    also move `powered by` text to team section
    
    * svg rendering through path prop
    
    * load all sponsor SVGs in DOM
    
    * logos responsive to tab-land
    
    * add anaglyph to footer text
    
    * undo anaglyph for better contrast
    
    * logos responsive to tab-port
    
    * logos fully responsive
    
    * background wire variant responsive with sponsors
    
    * kilo sponsors aligned
    
    * lint sponsors.scss
    
    * lint sponsors.scss
    
    * adjust background grid across hero & milestones
    
    * pass milestone props from controller
    
    * modularize JSON serialization in controller
    
    * lint sponsors.js
    
    * lint sponsors.js

--- src/views/Home/components/Hero/index.view.tsx
+++ src/views/Home/components/Hero/index.view.tsx
@@ -1,7 +1,14 @@
 import * as React from "react";
+import Lottie from "react-lottie";
+import { ReactComponent as Grid } from "images/components/hero/grid.svg";
+import { ReactComponent as Computer } from "images/components/hero/computer.svg";
+import { ReactComponent as Mouse } from "images/components/hero/mouse.svg";
+import { ReactComponent as LargeFlare } from "images/components/hero/ellipse1.svg";
+import { ReactComponent as MediumFlare } from "images/components/hero/ellipse2.svg";
+import { ReactComponent as SmallFlare } from "images/components/hero/ellipse3.svg";
 import PostcardStack from "components/PostcardStack/index.view";
-
 import "./Hero.scss";
+import animationData from "./postcard.json";
 
 type DescriptionDetail = {
   rightText: string;
@@ -28,6 +35,14 @@ interface HeroProps {
   children?: React.ReactNode;
 }
 
+const defaultOptions = {
+  animationData,
+  loop: false,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
+
 const Hero: React.FC<HeroProps> = ({
   pageName,
   title,
@@ -37,8 +52,18 @@ const Hero: React.FC<HeroProps> = ({
   return (
     <>
       <div className="Hero__container">
+        <Grid className="Hero__grid" />
+        <LargeFlare className="Hero__largeFlare" />
+        <MediumFlare className="Hero__mediumFlare" />
+        <SmallFlare className="Hero__smallFlare" />
+        <Computer className="Hero__computer" />
+        <Mouse className="Hero__mouse" />
         <div className="Hero__visual">
-          <PostcardStack pageName={pageName} />
+          {window.matchMedia("(prefers-reduced-motion: reduce)").matches ? (
+            <PostcardStack pageName={pageName} />
+          ) : (
+            <Lottie options={defaultOptions} />
+          )}
         </div>
         <div className="Hero__textContainer">
           {title.map((item) => (
