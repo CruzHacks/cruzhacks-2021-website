1082c100d94a44fdeabda9f46451fc346289c87b
Tim <tim.nguyen.cs@gmail.com>
2021-01-02 15:19:57 -0800

    added persistent menu button + fixed responsivness

--- src/components/Navbar/index.view.tsx
+++ src/components/Navbar/index.view.tsx
@@ -1,9 +1,13 @@
 import * as React from "react";
 import Button, { ButtonProps, ButtonTypes } from "components/Button/index.view";
+
 import IconButton from "@material-ui/core/IconButton";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import MenuRounded from "@material-ui/icons/MenuRounded";
+
+import { makeStyles } from "@material-ui/core";
+import Media from "react-media";
 import "./Navbar.scss";
 
 interface NavbarProps {
@@ -22,6 +26,16 @@ const Navbar: React.FC<NavbarProps> = ({ title, buttonProps }: NavbarProps) => {
     setAnchorEl(null);
   };
 
+  const useStyles = makeStyles({
+    buttonWrapper: {
+      margin: "0 calc(5% + var(--small-spacer)) 0 auto",
+      color: "var(--froly)",
+      "@media (max-width: 42.5em)": {
+        margin: "0 0 0 auto",
+      },
+    },
+  });
+
   return (
     <div className="Navbar">
       <div className="Navbar__title">{title}</div>
@@ -32,22 +46,15 @@ const Navbar: React.FC<NavbarProps> = ({ title, buttonProps }: NavbarProps) => {
             <Button key={entry.label} className="Navbar__button" {...entry} />
           ))}
       </div>
-      <div className="Navbar__buttonContainer--secondary">
-        {buttonProps
-          .filter((entry) => entry.type === ButtonTypes.SECONDARY)
-          .map((entry) => (
-            <Button key={entry.label} className="Navbar__button" {...entry} />
-          ))}
-      </div>
 
       <IconButton
         aria-label="more"
         aria-controls="long-menu"
         aria-haspopup="true"
-        className="Navbar__menuButton"
+        classes={{ root: useStyles().buttonWrapper }}
         onClick={handleClick}
       >
-        <MenuRounded />
+        <MenuRounded fontSize="large" />
       </IconButton>
       <Menu
         id="simple-menu"
@@ -59,18 +66,45 @@ const Navbar: React.FC<NavbarProps> = ({ title, buttonProps }: NavbarProps) => {
         anchorOrigin={{ vertical: "top", horizontal: "left" }}
         transformOrigin={{ vertical: "top", horizontal: "right" }}
       >
-        {buttonProps.map((entry) => (
-          <MenuItem key={entry.label}>
-            <a
-              className="Navbar__link"
-              href={entry.link}
-              target="_blank"
-              rel="noreferrer"
-            >
-              {entry.text}
-            </a>
-          </MenuItem>
-        ))}
+        <Media
+          queries={{
+            small: "max-width: 75em",
+            large: "min-width: 75em",
+          }}
+        >
+          {(matches) => (
+            <>
+              {matches.large &&
+                buttonProps
+                  .filter((entry) => entry.type === ButtonTypes.SECONDARY)
+                  .map((entry) => (
+                    <MenuItem key={entry.label}>
+                      <a
+                        className="Navbar__link"
+                        href={entry.link}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        {entry.text}
+                      </a>
+                    </MenuItem>
+                  ))}
+              {matches.small &&
+                buttonProps.map((entry) => (
+                  <MenuItem key={entry.label}>
+                    <a
+                      className="Navbar__link"
+                      href={entry.link}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {entry.text}
+                    </a>
+                  </MenuItem>
+                ))}
+            </>
+          )}
+        </Media>
       </Menu>
     </div>
   );
