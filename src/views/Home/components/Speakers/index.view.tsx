00b164c689c0daefd40e1c8dddf70f663764a0d0
Isai Lopez <ilopezro@ucsc.edu>
2021-01-07 00:33:22 -0800

    added speakers component and styling

--- src/views/Home/components/Speakers/index.view.tsx
+++ src/views/Home/components/Speakers/index.view.tsx
@@ -1,8 +1,34 @@
-import React from "react";
+import * as React from "react";
+import SpeakerCard from "components/SpeakerCard/index.view";
+import SpeakerBackground from "./background/index.view";
+import { SpeakerProp } from "views/Home/props/speakers";
 import "./speakers.scss";
 
-const SpeakersComponent: React.FC = () => {
-  return <div className="Speakers">Hello from SpeakersComponent</div>;
+interface SpeakerProps {
+  speakers: SpeakerProp[];
+  comingSoon: SpeakerProp[];
+}
+
+const SpeakersComponent: React.FC<SpeakerProps> = ({
+  speakers,
+  comingSoon,
+}: SpeakerProps) => {
+  return (
+    <div className="Speakers">
+      <SpeakerBackground />
+      <div className="Speakers__titleContainer">
+        <div className="Speakers__title">Keynote Speakers</div>
+      </div>
+      <div className="Speakers__cardsContainer">
+        {Object.entries(speakers).map(([type, props]) => (
+          <SpeakerCard {...props} key={type} />
+        ))}
+        {Object.entries(comingSoon).map(([type, props]) => (
+          <SpeakerCard {...props} key={type} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default SpeakersComponent;
