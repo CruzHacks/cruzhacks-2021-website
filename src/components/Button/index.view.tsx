fc83a32089fde7e93d7f3f3c1334882d6f8182fb
tim-nguyen-cs <33771623+tim-nguyen-cs@users.noreply.github.com>
2021-01-07 11:58:56 -0800

    navigation bar (#95)
    * first iteration of navigation bar
    
    * improved navbar responsiveness
    
    * remove transformY on button & improve animation
    
    * added hamburger menu
    
    * revised import and enum warnings
    
    * added persistent menu button + fixed responsivness
    
    * increased spacer size
    
    * simplied menu item rendering and reset packages
    
    * fixed compilation issues
    
    * optimize hamburger position to tab-port
    
    * fix unlintable typo in CSS serialization
    
    Co-authored-by: Eric Mockler <emockler@ucsc.edu>
    Co-authored-by: mocklee <mock7ee@gmail.com>

--- src/components/Button/index.view.tsx
+++ src/components/Button/index.view.tsx
@@ -1,10 +1,16 @@
 import * as React from "react";
 import "./Button.scss";
 
+export enum ButtonTypes {
+  PRIMARY = 0,
+  SECONDARY = 1,
+}
+
 export interface ButtonProps {
   text: string;
   link: string;
   label: string;
+  type: ButtonTypes;
   className?: string;
 }
 
