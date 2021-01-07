17bf6f1a8db7d08b95936d5c7788ebf7c1f2bf20
tim-nguyen-cs <33771623+tim-nguyen-cs@users.noreply.github.com>
2021-01-07 11:58:56 -0800

    navigation bar (#95)
    * first iteration of navigation bar
    
    * improved navbar responsiveness
    
    * remove transformY on button & improve animation
    
    * added hamburger menu
    
    * revised import and enum warnings
    
    * added persistent menu button + fixed responsivness
    
    * increased spacer size
    
    * simplied menu item rendering and reset packages
    
    * fixed compilation issues
    
    * optimize hamburger position to tab-port
    
    * fix unlintable typo in CSS serialization
    
    Co-authored-by: Eric Mockler <emockler@ucsc.edu>
    Co-authored-by: mocklee <mock7ee@gmail.com>

--- src/views/Home/index.view.tsx
+++ src/views/Home/index.view.tsx
@@ -1,27 +1,29 @@
 import * as React from "react";
-import Hero from "views/Home/components/Hero/index.view";
-import Button from "components/Button/index.view";
-import EmailSubscriptionForm from "components/EmailSubscription/index.view";
+import Navbar from "components/Navbar/index.view";
 import MLHBanner from "components/MLHBanner/index.view";
 import Background from "components/Background/index.view";
+import Hero from "views/Home/components/Hero/index.view";
+import EmailSubscriptionForm from "components/EmailSubscription/index.view";
 import Mission from "views/Home/components/Mission/index.view";
 import MilestonesComponent from "./components/Milestones/index.view";
+import PrizesComponent from "./components/Prizes/index.view";
+import FAQComponent from "./components/FAQ/index.view";
 import SponsorsComponent from "./components/Sponsors/index.view";
 import FooterComponent from "./components/Footer/index.view";
 import "./Home.scss";
 
+import { navbarProps } from "./props/navbar.js";
 import * as heroProps from "./props/hero.json";
 import * as missionProps from "./props/mission.json";
 import * as milestoneProps from "./props/milestones.json";
 import * as FAQprops from "./props/faq.json";
 import { sponsors } from "./props/sponsors.js";
 import { topics } from "./props/prizes";
-import FAQComponent from "./components/FAQ/index.view";
-import PrizesComponent from "./components/Prizes/index.view";
 
 const HomepageView: React.FC = () => {
   return (
     <>
+      <Navbar {...navbarProps} />
       <MLHBanner />
       <div className="Homepage">
         <Background>
@@ -33,17 +35,6 @@ const HomepageView: React.FC = () => {
             <div className="Homepage__emailSubscriptionContainer">
               <EmailSubscriptionForm />
             </div>
-            {Object.entries(heroProps.buttonProps).map(([type, props]) => (
-              <div key={type} className="Homepage__buttonContainer">
-                {Object.entries(props).map(([_, buttonProp]) => (
-                  <Button
-                    key={buttonProp.text}
-                    className={"Homepage__button"}
-                    {...buttonProp}
-                  />
-                ))}
-              </div>
-            ))}
           </Hero>
           <Mission
             about_text={missionProps.about}
