4182acedd8f61c20436d87404d368f4a11bb26bd
Isai Lopez <ilopezro@ucsc.edu>
2021-01-13 09:00:42 -0800

    added changed to arrow -- deleted old svg and using shared arrow

--- src/views/Team/components/Footer/index.view.tsx
+++ src/views/Team/components/Footer/index.view.tsx
@@ -1,16 +1,18 @@
 import * as React from "react";
-import { ReactComponent as Arrow } from "images/TeamPage/arrow.svg";
+import { ReactComponent as Arrow } from "images/arrow.svg";
 import "./Footer.scss";
 
 const Footer: React.FC = () => {
   return (
     <div className="TeamFooter">
-      <Arrow
-        className="TeamFooter__arrow"
-        onClick={() => {
-          window.scrollTo({ top: 0, behavior: "smooth" });
-        }}
-      />
+      <div className="TeamFooter__arrowContainer">
+        <Arrow
+          className="TeamFooter__arrow"
+          onClick={() => {
+            window.scrollTo({ top: 0, behavior: "smooth" });
+          }}
+        />
+      </div>
       <div className="TeamFooter__text">
         @ 2021 Cruzhacks. All rights reserved.
       </div>
