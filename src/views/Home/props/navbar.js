15e38046e7c6552704fb29c2249c3127283d36ed
William Gao <wlgao@ucsc.edu>
2021-01-13 21:35:09 -0800

    Add new sponsors. (#163)
    * Add new sponsors.
    
    * Update Choose Santa Cruz.
    
    * Tweaks.
    
    * Add CROSS.
    
    * Tweaks.
    
    * Oops.
    
    * Increase margins.
    
    * More changes.
    
    * preserve visual flow & patch logo visibility on iphoneX
    
    * patch typo in faq
    
    * hero occupies viewport
    
    * remove pre-event buttons
    
    * lint sponsors.scss
    
    * reposition schedule
    
    Co-authored-by: mocklee <mock7ee@gmail.com>
    Co-authored-by: Eric Mockler <emockler@ucsc.edu>

--- src/views/Home/props/navbar.js
+++ src/views/Home/props/navbar.js
@@ -3,26 +3,6 @@ import { ButtonTypes } from "components/Button/index.view";
 export const navbarProps = {
   title: "CruzHacks 2021",
   buttonProps: Object.values({
-    hacker: {
-      text: "Apply to hack!",
-      link: "https://bit.ly/cruzhacks-2021-hacker",
-      label: "hacker sign-up button",
-      type: ButtonTypes.PRIMARY,
-    },
-    judge_mentor: {
-      text: "Be a judge / mentor!",
-      link:
-        "https://docs.google.com/forms/d/e/1FAIpQLScVmW-gIcKGrp7fBVeH5i8G1VQ83IwmKrkIo_J2PeRj3PRYTw/viewform",
-      label: "judge/mentor sign-up button",
-      type: ButtonTypes.PRIMARY,
-    },
-    sponsor: {
-      text: "Be a sponsor!",
-      link:
-        "https://drive.google.com/file/d/127G6lE1SFVQUeC3wSMdMwn12E9sVFCyU/view",
-      label: "sponsor sign-up button",
-      type: ButtonTypes.PRIMARY,
-    },
     code_conduct: {
       text: "Our Code of Conduct",
       link: "http://mlh.io/code-of-conduct",
