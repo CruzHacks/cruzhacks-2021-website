b88bafc020eb77b642bb3e3fc0508bd06791a606
Isai Lopez <ilopezro@ucsc.edu>
2021-01-14 21:45:56 -0800

    fixed arrow size

--- src/views/Team/components/Footer/index.view.tsx
+++ src/views/Team/components/Footer/index.view.tsx
@@ -5,13 +5,13 @@ import "./Footer.scss";
 const Footer: React.FC = () => {
   return (
     <div className="TeamFooter">
-      <div className="TeamFooter__arrowContainer">
-        <Arrow
-          className="TeamFooter__arrow"
-          onClick={() => {
-            window.scrollTo({ top: 0, behavior: "smooth" });
-          }}
-        />
+      <div
+        className="TeamFooter__arrowContainer"
+        onClick={() => {
+          window.scrollTo({ top: 0, behavior: "smooth" });
+        }}
+      >
+        <Arrow className="TeamFooter__arrow" />
       </div>
       <div className="TeamFooter__text">
         @ 2021 Cruzhacks. All rights reserved.
