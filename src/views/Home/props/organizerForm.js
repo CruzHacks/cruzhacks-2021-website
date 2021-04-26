45b41f1f228c95582131395f10ab841b36afbabc
Krishna Kumar Pandian <krishnakpandian@gmail.com>
2021-04-26 13:49:46 -0700

    Updating deadline for CruzHacks 2022 (#236)
    * Updating deadline for CruzHacks 2022

--- src/views/Home/props/organizerForm.js
+++ src/views/Home/props/organizerForm.js
@@ -1,9 +1,9 @@
 import { ButtonTypes } from "components/Button/index.view";
 
 export const organizerFormProps = {
-  text: "Click Here to Apply to be a CruzHacks Organizer! Due 4/23",
+  text: "Click Here to Apply to be a CruzHacks Organizer! Due 4/30",
   link: "https://forms.gle/VJxrg8REVy3R55Cm8",
-  label: "Hello World",
+  label: "Organizer Apps",
   className: "Navbar__button",
   type: ButtonTypes.PRIMARY,
 };
