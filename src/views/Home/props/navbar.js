cb65581a5443a872172008e47744bf5a37bec93d
Krishna Pandian <krishnakpandian@gmail.com>
2021-09-13 19:22:07 -0700

    Making changes to 2021 for cornucopia

--- src/views/Home/props/navbar.js
+++ src/views/Home/props/navbar.js
@@ -1,7 +1,7 @@
 import { ButtonTypes } from "components/Button/index.view";
 
 export const navbarProps = {
-  title: "CruzHacks 2021",
+  title: "CruzHacks 2022",
   buttonProps: Object.values({
     code_conduct: {
       text: "Our Code of Conduct",
@@ -9,12 +9,12 @@ export const navbarProps = {
       label: "mlh code of conduct",
       type: ButtonTypes.SECONDARY,
     },
-    organizer_application: {
-      text: "Organizer Applications",
-      link: "https://forms.gle/VJxrg8REVy3R55Cm8",
-      label: "organizer app",
-      type: ButtonTypes.SECONDARY,
-    },
+    // organizer_application: {
+    //   text: "Organizer Applications",
+    //   link: "https://forms.gle/VJxrg8REVy3R55Cm8",
+    //   label: "organizer app",
+    //   type: ButtonTypes.SECONDARY,
+    // },
     contact_us: {
       text: "Contact Us",
       link: "mailto:contact@cruzhacks.com",
