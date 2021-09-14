b70c7517074459e059bbf235343a536d672712e0
Krishna Kumar Pandian <krishnakpandian@gmail.com>
2021-09-13 20:32:17 -0700

    Making changes to 2021 for cornucopia (#239)
    * Making changes to 2021 for cornucopia
    
    * Remove Deepcode

---
+++ src/views/Home/props/sponsorForm.js
@@ -0,0 +1,9 @@
+import { ButtonTypes } from "components/Button/index.view";
+
+export const sponsorFormProps = {
+  text: "Bring your Company to CruzHacks 2022!",
+  link: "mailto:sponsor@cruzhacks.com",
+  label: "Sponsor Application",
+  className: "Navbar__button",
+  type: ButtonTypes.PRIMARY,
+};
