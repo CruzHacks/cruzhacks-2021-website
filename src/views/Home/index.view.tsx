dede0220f744ed515c14c3147a5e3e47fbaf8c8e
Krishna Kumar Pandian <krishnakpandian@gmail.com>
2021-12-12 18:42:40 -0800

    Updating Home Page to Reevert 2022 Content to 2021 (#253)

--- src/views/Home/index.view.tsx
+++ src/views/Home/index.view.tsx
@@ -3,27 +3,27 @@ import Navbar from "components/Navbar/index.view";
 import MLHBanner from "components/MLHBanner/index.view";
 import Background from "components/Background/index.view";
 import Hero from "./components/Hero/index.view";
-import Button from "components/Button/index.view";
+// import Button from "components/Button/index.view";
 import EmailSubscriptionForm from "components/EmailSubscription/index.view";
 import Mission from "./components/Mission/index.view";
 import MilestonesComponent from "./components/Milestones/index.view";
-// import Schedule from "./components/Schedule/index.view";
-// import PrizesComponent from "./components/Prizes/index.view";
-// import SpeakersComponent from "./components/Speakers/index.view";
+import Schedule from "./components/Schedule/index.view";
+import PrizesComponent from "./components/Prizes/index.view";
+import SpeakersComponent from "./components/Speakers/index.view";
 import FAQComponent from "./components/FAQ/index.view";
-// import SponsorsComponent from "./components/Sponsors/index.view";
+import SponsorsComponent from "./components/Sponsors/index.view";
 import FooterComponent from "./components/Footer/index.view";
 
 import { navbarProps } from "./props/navbar.js";
 import * as heroProps from "./props/hero.json";
 import * as missionProps from "./props/mission.json";
 import * as milestoneProps from "./props/milestones.json";
-// import { topics } from "./props/prizes";
-// import { speakers } from "./props/speakers";
+import { topics } from "./props/prizes";
+import { speakers } from "./props/speakers";
 import * as FAQprops from "./props/faq.json";
-// import { sponsors } from "./props/sponsors.js";
+import { sponsors } from "./props/sponsors.js";
 import { footerProps } from "./props/footer";
-import { sponsorFormProps } from "./props/sponsorForm";
+// import { sponsorFormProps } from "./props/sponsorForm";
 
 import "./Home.scss";
 
@@ -42,24 +42,24 @@ const HomepageView: React.FC = () => {
             <div className="Homepage__emailSubscriptionContainer">
               <EmailSubscriptionForm />
             </div>
-            <div className="Homepage__sponsorApplication">
+            {/* <div className="Homepage__sponsorApplication">
               <Button {...sponsorFormProps} />
-            </div>
+            </div> */}
           </Hero>
           <Mission
             about_text={missionProps.about}
             mission_text={missionProps.mission}
           />
           <MilestonesComponent milestones={milestoneProps.milestones} />
-          {/* <PrizesComponent topics={topics} />
+          <PrizesComponent topics={topics} />
           <SpeakersComponent speakers={speakers} />
-          <Schedule /> */}
+          <Schedule />
           <FAQComponent
             questionAnswersColumnLeft={FAQprops.questionAnswersColumnLeft}
             questionAnswersColumnRight={FAQprops.questionAnswersColumnRight}
             questionAnswersColumnMiddle={FAQprops.questionAnswersColumnMiddle}
           />
-          {/* <SponsorsComponent sponsors={sponsors} /> */}
+          <SponsorsComponent sponsors={sponsors} />
           <FooterComponent links={footerProps} />
         </Background>
       </div>
